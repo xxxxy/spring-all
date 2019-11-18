a1fe46e03c3460596555e0ae0af14f40df6cb264
ex_huangjy5 <jingyang1.huang@partner.midea.com>
2019-11-18 21:10:46 +0800

    add spring-boot-json

---
+++ spring-boot-json/src/main/java/com/john/springbootjson/SpringBootJsonApplication.java
@@ -0,0 +1,13 @@
+package com.john.springbootjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJsonApplication.class, args);
+    }
+
+}
