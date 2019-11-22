32d13b9f8eca2155273210a7729e16ec73c21ec8
ex_huangjy5 <jingyang1.huang@partner.midea.com>
2019-11-22 11:05:55 +0800

    add spring-boot-upload 文件上传

---
+++ spring-boot-exception/src/test/java/com/john/springbootexception/SpringBootExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.john.springbootexception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
